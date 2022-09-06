bf18a9f5ce77d4c47f62880bedf5e5ce2657c3fd
Rajavel-78 <112758108+Rajavel-78@users.noreply.github.com>
2022-09-06 19:13:14 +0530

    Add files via upload

---
+++ LearnShiftOperator.java
@@ -0,0 +1,8 @@
+class LearnShiftOPerator{
+	public static void main(String[] args) {
+		System.out.println("Left shift "+(10>>2));
+		System.out.println("right shift "+(10<<2));
+		System.out.println("Unsigned rightShift "+(10>>>2));
+
+	}
+}
